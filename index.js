865609ce339e457e2a1934d48f5c5cdb65eeb65c
Darwin Skenderasi <shivermytimbers123@outlook.com>
2023-11-27 18:49:35 +0000

    shimi

--- index.js
+++ index.js
@@ -1,66 +1,53 @@
-import express from "express";
-import http from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
-import path from "node:path";
-import * as dotenv from "dotenv";
-dotenv.config();
+import express from 'express';
+import http from 'node:http';
+import createBareServer from "educational-br-sr";
+import path from 'node:path';
+import cors from 'cors';
 
 const __dirname = process.cwd();
 const server = http.createServer();
 const app = express(server);
-const bareServer = createBareServer("/bare/");
+const bareServer = createBareServer('/outerspace/');
+const PORT = 8080;
 
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.use(express.static(path.join(__dirname, "static")));
+const routes = [
+  { path: '/', file: 'index.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/events', file: 'games.html' },
+  { path: '/diagnostic', file: 'settings.html' },
+  { path: '/local-news', file: 'tabs.html' },
+  { path: '/image-galleries', file: 'go.html' },
+];
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "index.html"));
+app.get('/edu/*', cors({ origin: false }), async (req, res, next) => {
+  try {
+    const reqTarget = `https://raw.githubusercontent.com/InterstellarNetwork/Interstellar-Assets/main/${req.params[0]}`;
+    const asset = await fetch(reqTarget);
+    
+    if (asset.ok) {
+      const data = await asset.arrayBuffer();
+      res.end(Buffer.from(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error('Error fetching:', error);
+    next(error);
+  }
 });
 
-app.get("/photography", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "search.html"));
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', route.file));
+  });
 });
 
-app.get("/nature", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "play.html"));
-});
-
-app.get("/forest", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "widgetbot.html"));
-});
-
-app.get("/go", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "go.html"));
-});
-
-app.get("/settings", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "settings.html"));
-});
-
-app.get("/donate", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "donate.html"));
-});
-
-app.get("/ocean", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "apps.html"));
-});
-
-app.get("/404", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "404.html"));
-});
-
-app.get("/*", (req, res) => {
-  res.redirect("/404");
-});
-
-// Bare Server
-server.on("request", (req, res) => {
+server.on('request', (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
   } else {
@@ -68,7 +55,7 @@ server.on("request", (req, res) => {
   }
 });
 
-server.on("upgrade", (req, socket, head) => {
+server.on('upgrade', (req, socket, head) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeUpgrade(req, socket, head);
   } else {
@@ -76,10 +63,10 @@ server.on("upgrade", (req, socket, head) => {
   }
 });
 
-server.on("listening", () => {
-  console.log(`Interstellar running at http://localhost:${process.env.PORT}`);
+server.on('listening', () => {
+  console.log(`Running at http://localhost:${PORT}`);
 });
 
 server.listen({
-  port: process.env.PORT,
+  port: PORT,
 });
